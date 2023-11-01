469b02caf8285bb2bfe6a4b42d0754e680b1b52e
Astoner <aidanstoner1@gmail.com>
2023-11-01 19:44:16 -0400

    Started initializations of USART

--- Final_pt2.c
+++ Final_pt2.c
@@ -7,13 +7,22 @@
 
 //******************************************************************************
 //Functions
-void initUART();
+void initUSART();
 //******************************************************************************
 //Main Fucntion
 void main() {
 
 }
 
-void initUART(){
-
+void initUSART(){
+     RCC_APB2ENR |= 1; //start clock to PA9 and PA10 can use alternate function
+     AFIO_MAPR = 0xF000000; //do no want to remap PA9 and PA10 in bit 2
+     RCC_APB2ENR |= 1 << 2; //enable clock for PA9 and PA10
+     GPIOA_CRH = 0x4B0; //sets PA9 (Tx) as a push-pull output and PA10 (Rx) as an input
+     RCC_APB2ENR |= 1<<14; //enable clock for USART1
+     USART1_BRR=0X00000506; //set baud rate to 56000
+     USART1_CR1.B12 = 0; //forces M as 0 so 8 data bits
+     USART1_CR2.B12 = 0; //forces bits 13 and 12 to 00 so there is one stop bit
+     USART1_CR2.B13 = 0;
+     USART1_CR3.B8 = 0; //forces bit 8 to 0 so no hardware flow
 }
