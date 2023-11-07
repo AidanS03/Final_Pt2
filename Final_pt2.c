dfb7cf35e579c98cf0add1b61a5b498e2c71c51a
Astoner <aidanstoner1@gmail.com>
2023-11-06 21:13:23 -0500

    UART works idk about my init, currently the board send the output of joyRead()

--- Final_pt2.c
+++ Final_pt2.c
@@ -4,14 +4,48 @@
 //Description:
 //******************************************************************************
 //Global Varibles
-
+int joy;
 //******************************************************************************
 //Functions
 void initUSART();
+int joyRead();
+void InitializeUSART1();
+void initGPIO();
 //******************************************************************************
 //Main Fucntion
 void main() {
+     for(;;){
+          initGPIO();
+//Objective 2
+          //initUSART();
+          InitializeUSART1();
+          joy = joyRead();
+          if(USART1_SR.B7 == 1){
+               USART1_DR = joy;
+          }
+     }
+}
+//******************************************************************************
+//Function Definitions
+int joyRead(){
+     GPIOA_CRL |= 1 << 26; //Sets PA6 as an input
+     GPIOB_CRL |= 1 << 22; //sets PB5 as an input
+     GPIOC_CRH |= 1 << 22; //sets PC13 as an input
+     GPIOE_CRH = 0x33333333; //Set PortE/H as an output for LEDS
+     GPIOD_CRL |= 0x40400; //Sets PD2 and PD4 as an input
 
+
+     if(GPIOD_IDR.B4 == 0){
+          return 1; //joystick up return a 1
+     }else if(GPIOA_IDR.B6 == 0){
+          return 2; //joystick right return 2
+     }else if(GPIOB_IDR.B5 == 0){
+          return 3; //joystick down return 3
+     }else if(GPIOD_IDR.B2 == 0){
+          return 4; //joystick left return 4
+     }else if(GPIOC_IDR.B13 == 0){
+          return 5; //joystick clicked return 5
+     }else return 0; //nothing pressed return 0
 }
 
 void initUSART(){
@@ -29,4 +63,37 @@ void initUSART(){
      USART1_CR1.B2 = 1; //Rx enabled
      USART1_CR1.B3 = 1; //Tx enabled
      USART1_CR1.B13 = 1; //Enables UART and needs to be enabled after all the configuration above
+}
+
+void InitializeUSART1(){ // Sub function which initializes the registers to enable USART1
+        RCC_APB2ENR |= 1;                 // Enable clock for Alt. Function. USART1 uses AF for PA9/PA10
+        AFIO_MAPR=0X0F000000;             // Do not mask PA9 and PA10 (becaue we are using for USART)
+        RCC_APB2ENR |= 1<<2;              // Enable clock for GPIOA
+        GPIOA_CRH &= ~(0xFF << 4);        // Clear PA9, PA10
+        GPIOA_CRH |= (0x0B << 4);         // USART1 Tx (PA9) output push-pull
+        GPIOA_CRH |= (0x04 << 8);         // USART1 Rx (PA10) input floating
+        RCC_APB2ENR |= 1<<14;             // enable clock for USART1
+        USART1_BRR=0X00000506;            // Set baud rate to 56000
+        // Per data sheet (pg. 1010) USART1_CR1 consists of the following:
+        //13 12   11  10  9    8     7    6      5      4  3  2   1   0
+        //UE  M WAKE PCE PS PEIE TXEIE TCIE RXNEIE IDLEIE TE RE RWU SBK
+        //rw rw  rw   rw rw   rw    rw   rw     rw     rw rw rw  rw  rw
+        USART1_CR1 &= ~(1<<12);          // Force 8 data bits. M bit is set to 0.
+        USART1_CR2 &= ~(3<<12);          // Force 1 stop bit
+        USART1_CR3 &= ~(3<<8);           // Force no flow control and no DMA for USART1
+        USART1_CR1 &= ~(3<<9);           // Force no parity and no parity control
+        USART1_CR1 |= 3<<2;              // RX, TX enable
+        //The following two instructions can also be used to enable RX and TX manually
+        //USART1_CR1.TE=1; //TX enable
+        //USART1_CR1.RE=1; //RX enable
+        USART1_CR1 |= 1<<13;            // USART1 enable. This is done after configuration is complete
+        Delay_ms(100);                  // Wait for USART to complete configuration and enable. This is
+                                                                        // not always necessary, but good practice.
+}
+void initGPIO(){
+     RCC_APB2ENR |= 1 << 2;  //enables clock for PortA
+     RCC_APB2ENR |= 1 << 3;  //enables clock for PortB
+     RCC_APB2ENR |= 1 << 4;  //enables clock for PortC
+     RCC_APB2ENR |= 1 << 5;  //enables clock for PortD
+     RCC_APB2ENR |= 1 << 6;  //enables clock for PortE
 }
