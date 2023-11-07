841900f173e2cb49111bc3d103dbe7d8141121f3
Shaunessy <sreynoldsschool1@gmail.com>
2023-11-06 19:49:02 -0500

    my fisrt attempts at psuedocodeing stuff rn

--- Final_pt2.c
+++ Final_pt2.c
@@ -1,13 +1,23 @@
 //Name:
 //Date:
 //Course:
-//Description:
+//Description:        
+
+ //Objective 2:
+
+//Write a program which communicates with the USART terminal using a baud rate of 56,000. 
+//When the user interacts with the Joystick, the MCU will write to the terminal the 
+//ASCII characters “UP Pressed”, “DN Pressed”, “LT Pressed”, “RT Pressed ”, or “CK Pressed”, 
+//depending on which direction was pressed
+
+
 //******************************************************************************
 //Global Varibles
 
 //******************************************************************************
 //Functions
 void initUART();
+
 //******************************************************************************
 //Main Fucntion
 void main() {
@@ -15,5 +25,12 @@ void main() {
 }
 
 void initUART(){
-
+      for(;;){
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      
+      }
 }
