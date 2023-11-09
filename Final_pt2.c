d7c297384af5f4d654cdf8c7dfe563d0e6bf1d8c
Astoner <aidanstoner1@gmail.com>
2023-11-08 20:35:14 -0500

    my void initUART() function works we can use that now

--- Final_pt2.c
+++ Final_pt2.c
@@ -9,7 +9,6 @@ int joy;
 //Functions
 void initUSART();
 int joyRead();
-void InitializeUSART1();
 void initGPIO();
 //******************************************************************************
 //Main Fucntion
@@ -68,31 +67,6 @@ void initUSART(){
      Delay_ms(100);
 }
 
-void InitializeUSART1(){ // Sub function which initializes the registers to enable USART1
-        RCC_APB2ENR |= 1;                 // Enable clock for Alt. Function. USART1 uses AF for PA9/PA10
-        AFIO_MAPR=0X0F000000;             // Do not mask PA9 and PA10 (becaue we are using for USART)
-        RCC_APB2ENR |= 1<<2;              // Enable clock for GPIOA
-        GPIOA_CRH &= ~(0xFF << 4);        // Clear PA9, PA10
-        GPIOA_CRH |= (0x0B << 4);         // USART1 Tx (PA9) output push-pull
-        GPIOA_CRH |= (0x04 << 8);         // USART1 Rx (PA10) input floating
-        RCC_APB2ENR |= 1<<14;             // enable clock for USART1
-        USART1_BRR=0X00000506;            // Set baud rate to 56000
-        // Per data sheet (pg. 1010) USART1_CR1 consists of the following:
-        //13 12   11  10  9    8     7    6      5      4  3  2   1   0
-        //UE  M WAKE PCE PS PEIE TXEIE TCIE RXNEIE IDLEIE TE RE RWU SBK
-        //rw rw  rw   rw rw   rw    rw   rw     rw     rw rw rw  rw  rw
-        USART1_CR1 &= ~(1<<12);          // Force 8 data bits. M bit is set to 0.
-        USART1_CR2 &= ~(3<<12);          // Force 1 stop bit
-        USART1_CR3 &= ~(3<<8);           // Force no flow control and no DMA for USART1
-        USART1_CR1 &= ~(3<<9);           // Force no parity and no parity control
-        USART1_CR1 |= 3<<2;              // RX, TX enable
-        //The following two instructions can also be used to enable RX and TX manually
-        //USART1_CR1.TE=1; //TX enable
-        //USART1_CR1.RE=1; //RX enable
-        USART1_CR1 |= 1<<13;            // USART1 enable. This is done after configuration is complete
-        Delay_ms(100);                  // Wait for USART to complete configuration and enable. This is
-                                                                        // not always necessary, but good practice.
-}
 void initGPIO(){
      RCC_APB2ENR |= 1 << 2;  //enables clock for PortA
      RCC_APB2ENR |= 1 << 3;  //enables clock for PortB
